6028fe7da055c94a21ddec8ce0d415d4e79ffe5e
Daniella Mesquita <mesquitadaniella@proton.me>
2022-12-02 01:59:52 -0300

    Don't attempt to create folders/files that already exist

--- install.sh
+++ install.sh
@@ -1,18 +1,16 @@
 #!/bin/bash
 
 SCRIPTPATH="$( cd -- "$(dirname "$0")" >/dev/null 2>&1 ; pwd -P )"
+flouser=$(logname)
 
 is_root=false
-
 if [ "$([[ $UID -eq 0 ]] || echo "Not root")" = "Not root" ]
    then
       is_root=false
    else
       is_root=true
 fi
-
 maysudo=""
-
 if [ "$is_root" = "false" ]
    then
       maysudo="sudo"
@@ -23,14 +21,11 @@ fi
 echo "Installing NodeJS..."
 $maysudo apt-get install nodejs npm -y
 
-echo "Creating HTML5 apps directory..."
-$maysudo mkdir /1/apps
+if [ ! -e /1/apps ]; then echo "Creating HTML5 apps directory...";$maysudo mkdir /1/apps; fi
 
-echo "Creating HTML5 games directory..."
-$maysudo mkdir /1/games
+if [ ! -e /1/games ]; then echo "Creating HTML5 games directory...";$maysudo mkdir /1/games; fi
 
-echo "Creating html5 directory..."
-$maysudo mkdir /1/html5
+if [ ! -e /1/html5 ]; then echo "Creating html5 directory...";$maysudo mkdir /1/html5; fi
 echo "Adding Color Converter.html5..."
 cp -f include/html5/Color\ Converter.html5 /1/html5/
 
@@ -77,26 +72,23 @@ $maysudo update-mime-database /usr/share/mime
 $maysudo gtk-update-icon-cache /usr/share/icons/gnome/ -f
 
 echo "Installing icons for .apps, .game and .html5 files..."
-
-git clone https://github.com/DaniellMesquita/linux-icon-builder.git
-
+git clone https://github.com/Floflis/linux-icon-builder.git
 cd linux-icon-builder
 sh ./linux-icon-builder "$SCRIPTPATH/include/icons/application-x-apps.png" "mimetypes" "application-x-apps.png"
 sh ./linux-icon-builder "$SCRIPTPATH/include/icons/application-x-game.png" "mimetypes" "application-x-game.png"
 sh ./linux-icon-builder "$SCRIPTPATH/include/icons/application-x-html5.png" "mimetypes" "application-x-html5.png"
 cd icons
-
+#-
 if [ ! -e /usr/share/icons/Floflis ]; then
    cp -r -f --preserve=all . /usr/share/icons/Yaru/
 else
    cp -r -f --preserve=all . /usr/share/icons/ubuntu/Yaru/
 fi
-
+#-
 cd "$SCRIPTPATH"
 rm -rf linux-icon-builder
 
 echo "Installing handler for .apps, .game and .html5 files..."
-
 git clone https://github.com/Floflis/floflis-application-handler.git
 cd floflis-application-handler
 $maysudo bash install.sh
@@ -104,9 +96,9 @@ cd "$SCRIPTPATH"
 rm -rf floflis-application-handler
 
 echo "Installing global shared NodeJS modules..."
-$maysudo mkdir /1
-$maysudo mkdir /1/Floflis
-$maysudo mkdir /1/Floflis/libs
+if [ ! -e /1 ]; then $maysudo mkdir /1; fi
+if [ ! -e /1/Floflis ]; then $maysudo mkdir /1/Floflis; fi
+if [ ! -e /1/Floflis/libs ]; then $maysudo mkdir /1/Floflis/libs; fi
 tar -C /1/Floflis/libs -xzf include/node_modules.tar.gz
 cd /1/Floflis/libs
 npm install
@@ -116,8 +108,7 @@ cd "$SCRIPTPATH"
 echo "They'll be useful for Floflis Central and other apps/games made with the C2 engine."
 
 # HOME LAYER -->
-echo "Installing game-engines lib folder..."
-$maysudo mkdir /1/Floflis/libs/game-engines
+if [ ! -e /1/Floflis/libs/game-engines ]; then echo "Creating game-engines lib folder...";$maysudo mkdir /1/Floflis/libs/game-engines; fi
 echo "Installing C2 common libs..."
 tar -C /1/Floflis/libs/game-engines -xzf include/Floflis_libs_game-engines_c2.tar.gz
 echo "Having common libs, C2 games/apps will be smaller to download (also to store locally)!"
